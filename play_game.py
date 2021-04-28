033a785283fd6b300d44bcc686c5061c5569ed9c
Trevor Freeman <tefreeman@crimson.ua.edu>
2021-04-28 16:52:53 -0500

    sas

--- play_game.py
+++ play_game.py
@@ -42,11 +42,9 @@ class GameReader():
         time.sleep(1)
     
     def attached_target_low(self, target = ""):
-        tar_coords = GameReader.hp_bars[target]
-        tar_coords["coords"][0] = tar_coords["coords"][0] + round(tar_coords["w"]/1.75)
-        tar_coords["coords"][1] = tar_coords["coords"][1] + 1
+        tar_coords = (GameReader.hp_bars[target]["coords"][0]+ round(GameReader.hp_bars["w"]/1.75), GameReader.hp_bars["coords"][1] + 1)
         
-        if Color_Lib.match_color_screen(tar_coords["coords"], GameReader.hp_bars["color"]):
+        if Color_Lib.match_color_screen(tar_coords, GameReader.hp_bars["color"]):
             return True
         
         return False
